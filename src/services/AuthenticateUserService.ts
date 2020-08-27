8867d9af9b372996ce25143ece7c2d8c7c943e07
MayronSouza <mayron.souza@gmz.solutions>
2020-08-27 18:23:29 -0300

    Inserindo os conceitos de JWT no service de autenticacao de usuario

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,44 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
 interface RequestDTO {
   email: string;
   password: string;
 }
 
+interface ResponseDTO {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: RequestDTO): Promise<void> {}
+  public async execute({ email, password }: RequestDTO): Promise<ResponseDTO> {
+    const userReository = getRepository(User);
+
+    const user = await userReository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, '045885ab5b541970a82dff775474ed76', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
 }
 
 export default AuthenticateUserService;
