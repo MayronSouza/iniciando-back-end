37d22677c596b33c1d8352c36ce1619f8cc68a8e
MayronSouza <mayron.souza@gmz.solutions>
2020-09-27 12:46:03 -0300

    IMplementando teste de criaçao de usuario

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+import CreateAppointmetService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointmet', () => {
+  it('should be able to create a appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmetService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  it('should not be able to create two apoointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmetService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2010, 11, 2, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123124',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
-import appointmentsRouter from '@modules/appointments/infra/typeorm/entities/appointments.routes';
-import usersRouter from '@modules/users/infra/http/users.routes';
-import sessionsRouter from '@modules/users/infra/http/sessions.routes';
+import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
