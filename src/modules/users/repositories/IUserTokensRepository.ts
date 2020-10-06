800df450fa35ed34d9a56c367c7b8ad3eeaf8633
MayronSouza <mayron.souza@gmz.solutions>
2020-10-06 11:57:03 -0300

    Implementando as rotas e controllers referentes a recuparaçao de senha, bem como, que os tokens sejam salvos no DB: criando tabela, repositorio e registrando este repositorio no container para injeçao de dependencia

--- src/modules/users/repositories/IUserTokensRepository.ts
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -2,5 +2,5 @@ import UserTonken from '../infra/typeorm/entities/UserToken';
 
 export default interface IUserTokensRepository {
   generate(user_id: string): Promise<UserTonken>;
-  findBytoken(token: string): Promise<UserTonken | undefined>;
+  findByToken(token: string): Promise<UserTonken | undefined>;
 }
